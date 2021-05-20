03d39beaca132ff1e0398de28bfdc9eac69729be
igrekus <upload234@mail.ru>
2021-05-20 18:36:15 +0300

    FIX: ibcrease palettes size to fix stat label rendering

--- primaryplotwidget.py
+++ primaryplotwidget.py
@@ -7,7 +7,8 @@ from PyQt5.QtCore import Qt
 # https://www.learnpyqt.com/tutorials/plotting-pyqtgraph/
 # https://pyqtgraph.readthedocs.io/en/latest/introduction.html#what-is-pyqtgraph
 
-colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf',
+          '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf',]
 
 
 class PrimaryPlotWidget(QWidget):
